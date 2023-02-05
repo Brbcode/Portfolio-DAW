17e5d0f6b44775b998e3f6bcaba66266d677e827
Bruno <brbcode@gmail.com>
2023-02-05 11:26:19 +0100

    Feature app inner loader

--- assets/js/App.jsx
+++ assets/js/App.jsx
@@ -1,19 +1,23 @@
 import React, {Suspense} from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { Outlet } from 'react-router-dom';
 
 
 import '../styles/styles.scss';
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import SVG from "react-inlinesvg";
 
 export default class App extends React.Component {
     render() {
         return (<>
                 <Header />
                 <main>
-                    <Suspense fallback={<FontAwesomeIcon icon={faSpinner} className='fa-spin app-inner-load' />}>
+                    <Suspense
+                        fallback={
+                        <section id='route-inner-load'>
+                            <SVG src='build/images/favicon/favicon.svg'/>
+                        </section>
+                    }>
                         <Outlet />
                     </Suspense>
                 </main>
